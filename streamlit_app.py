2bd798b271b5788e0c97d14b8f76ddce65b7bc19
Barry Xu <123760731+GDarknessX@users.noreply.github.com>
2023-08-09 20:48:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,21 @@
 # Pulsewave
 import streamlit as st
 
-st.write("Hello World")
+def main():
+    st.title("选择题网站")
+
+    # 问题和选项
+    question = "哪种编程语言是最流行的？"
+    option_a = "Python"
+    option_b = "Java"
+
+    st.write(question)
+
+    # 显示选项按钮
+    if st.button(option_a):
+        st.write(f"您选择了 {option_a}。")
+    elif st.button(option_b):
+        st.write(f"您选择了 {option_b}。")
+
+if __name__ == "__main__":
+    main()
