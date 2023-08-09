5c8fff52bde83178acef5278ae86fbd65843aca4
Barry Xu <123760731+GDarknessX@users.noreply.github.com>
2023-08-09 21:58:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -398,6 +398,8 @@ def main():
             st.experimental_rerun()
 
 if __name__ == "__main__":
+    if 'page' not in st.session_state:
+        st.session_state.page = "initial"
     if 'question' not in st.session_state:
         st.session_state.question = "a"
     main()
