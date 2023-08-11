62939604562ea028ee17a43aef14a8b14d493d8e
Barry Xu <123760731+GDarknessX@users.noreply.github.com>
2023-08-11 12:55:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,6 @@ st.title("健康小问题")
 
 def main():
 
-# 获取传递的变量
-    #variable_from_url = st.experimental_get_query_params().get("variable", [0])[0]
     
     if variable_from_url == "0":
         variable_from_url = "X"
