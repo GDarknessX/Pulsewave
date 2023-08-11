6e6476511cb3c4b5ec0f44c0b2c83eb0eb7aa2ea
Barry Xu <123760731+GDarknessX@users.noreply.github.com>
2023-08-11 16:09:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,22 +46,22 @@ def main():
         {
             "question": "你长期从事体力劳动或高强度运动吗？",
             "options": ["是", "否"],
-            "answer": 
+            "answer": ""
         },
         {
             "question": "你最近排便顺畅吗？",
             "options": ["是", "否"],
-            "answer": 
+            "answer": ""
         },
         {
             "question": "你常居住在高海拔区？",
             "options": ["是", "否"],
-            "answer": 
+            "answer": ""
         },
         {
             "question": "你最近有吹冷风着凉的经历吗？",
             "options": ["是", "否"],
-            "answer":
+            "answer":""
         }
     ]
     questions_fu = [
@@ -69,73 +69,73 @@ def main():
         {
             "question": "你感到冷/热吗？",
             "options": ["冷", "热"],
-            "answer":
+            "answer":""
         },
         {
             "question": "你比较胖/瘦？",
             "options": ["胖", "瘦"],
-            "answer":
+            "answer":""
         },
         {
             "question": "你是否有长期的疾病史？",
             "options": ["是", "否"],
-            "answer":
+            "answer":""
         }
     ]
     questions_shi = [
         {
             "question": "你最近是否发烧或者感冒过？",
             "options": ["是", "否"],
-            "answer":
+            "answer":""
         },
         {
             "question": "你喜欢喝酒吗？",
             "options": ["是", "否"],
-            "answer":
+            "answer":""
         },
         {
             "question": "你经常熬夜吗？",
             "options": ["是", "否"],
-            "answer":
+            "answer":""
         },
         {
             "question": "你最近总是发脾气吗？",
             "options": ["是", "否"],
-            "answer":
+            "answer":""
         }
     ]
     questions_shu = [
         {
             "question": "你感觉到热吗？",
             "options": ["是", "否"],
-            "answer":
+            "answer":""
         },
         {
             "question": "你晚上睡觉流汗吗？",
             "options": ["是", "否"],
-            "answer":
+            "answer":""
         },
         {
             "question": "你常常感觉气短心跳加速吗？",
             "options": ["是", "否"],
-            "answer": 
+            "answer": ""
         }
     ]
     questions_xu = [
         {
             "question": "你最近有消化不良吗？",
             "options": ["是", "否"],
-            "answer":
+            "answer":""
         },
         {
             "question": "你贫血吗？",
             "options": ["是", "否"],
-            "answer":
+            "answer":""
         },
         {
             "question": "你瘦吗？",
             "options": ["是", "否"],
-            "answer":
+            "answer":""
         }
     ]
 
@@ -248,6 +248,4 @@ def main():
         st.write("您已完成所有选项。")
 
 if __name__ == "__main__":
-    if 'question' not in st.session_state:
-        st.session_state.question = "e"
 main()
